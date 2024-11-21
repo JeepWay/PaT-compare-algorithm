ebf5648a5bc8af6db2df862d85ce79150f43f1b8
JeepWay <william.liu0731@gmail.com>
2024-11-22 01:08:28 +0800

    docs: add main hyper settings and script

---
+++ zhao-2d/scripts/all.sh
@@ -0,0 +1,5 @@
+# main settings
+python main.py --config_path settings/v1_zhao_ACKTR-h200-n64-M7-rA.yaml
+python main.py --config_path settings/v2_zhao_ACKTR-h400-n64-M7-rA.yaml
+python main.py --config_path settings/v3_zhao_ACKTR-h1600-n64-M7-rA.yaml
+python main.py --config_path settings/v4_zhao_ACKTR-h1600-n64-M7-rA.yaml
