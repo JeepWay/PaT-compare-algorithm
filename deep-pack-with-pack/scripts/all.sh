29e139544f6d610b35545498acb7ac1123dc541e
JeepWay <william.liu0731@gmail.com>
2024-12-26 00:31:41 +0800

    docs: add run script and gitignore

---
+++ deep-pack-with-pack/scripts/all.sh
@@ -0,0 +1,5 @@
+# main settings
+python main.py --config_path settings/v1_deeppack_mask_DDQN-h200-rC.yaml
+python main.py --config_path settings/v2_deeppack_mask_DDQN-h400-rC.yaml
+python main.py --config_path settings/v3_deeppack_mask_DDQN-h1600-rC.yaml
+python main.py --config_path settings/v4_deeppack_mask_DDQN-h1600-rC.yaml
